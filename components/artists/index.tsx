c3f1aba97d67bfff805ee893df36d9dff3049d74
LuciaSepulveda <luciabsep18@gmail.com>
2025-05-27 19:55:25 -0300

    update package

--- components/artists/index.tsx
+++ components/artists/index.tsx
@@ -68,6 +68,7 @@ const Artists = ({ artists }: Props) => {
               new THREE.Vector3(0, 0.5, 0),
             ]}
             position={[i * 0.06 - urls.length * 0.03, -height / 2 + 0.6, 0]}
+            derivatives={[]}
           />
         ))}
       </group>
